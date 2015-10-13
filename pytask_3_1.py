da2f36f16f9fe8a964ecffeb292fb8fcb7fb6e9c
Alex Klimovich <alex_klimovich@yahoo.com>
2015-10-13 10:33:57 +0300

    task 3 - subtask 1

--- pytask_3_1.py
+++ pytask_3_1.py
@@ -1,12 +1,19 @@
 class Fake(object):
-    """
-    >>> fake = Fake()
-    >>> fake.non_existing_method('asdfa')
-    >>> fake.attribute
-    >>> fake[4]
-    >>> fake['non existing key']
-    >>> fake2 = fake.blablabla()
-    >>> fake2.some_name()
-    >>> fake2.whatever.again_whatever().and_again['yury'][1]
-    """
-    pass
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def __getattr__(self, item):
+        return self
+
+    def __getitem__(self, item):
+        return self
+
+
+fake = Fake()
+fake.non_existing_method('asdfa')
+fake.attribute
+fake[4]
+fake['non existing key']
+fake2 = fake.blablabla()
+fake2.some_name()
+fake2.whatever.again_whatever().and_again['yury'][1]
